456eca8676c4b029870f287a85d9ea688952cd56
hai14894 <hohoanghai14894@gmail.com>
2020-06-19 09:57:54 +1000

    create border around the cells

--- tictactoe.js
+++ tictactoe.js
@@ -9,15 +9,19 @@ function drawGrid(){
             let col = document.createElement("td")
             col.innerHTML = counter
             counter++
+            col.style.borderStyle = "solid"
             row.appendChild(col)
+
         }
         table.appendChild(row)
     }
+    
 }
+drawGrid()
 //create the combinations of a winner
 let winners = [
     [1,2,3], [4,5,6], [7,8,9], //Horizontal
     [1,4,7], [2,5,8], [3,6,9], //Vertical
     [1,5,9], [3,5,7] //Diagonal
 ]
-console.log(winners);
+
