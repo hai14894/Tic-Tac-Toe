b2d7a9e17c88e3425e4b11b0cc728a2fe76ebc62
hai14894 <hohoanghai14894@gmail.com>
2020-06-19 09:23:16 +1000

    create the winners combinations

--- tictactoe.js
+++ tictactoe.js
@@ -14,4 +14,10 @@ function drawGrid(){
         table.appendChild(row)
     }
 }
-drawGrid()
+//create the combinations of a winner
+let winners = [
+    [1,2,3], [4,5,6], [7,8,9], //Horizontal
+    [1,4,7], [2,5,8], [3,6,9], //Vertical
+    [1,5,9], [3,5,7] //Diagonal
+]
+console.log(winners);
